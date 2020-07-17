a1cfc6c08ebc4de93d0f10686ac864c53b190ec0
webdevjscoder <cobianjuan19@gmail.com>
2020-07-17 10:18:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+});
 }
+
+addingEventListener();
